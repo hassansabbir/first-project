dc8e925f38862b0ad06a6320dc27787f4f413609
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-11-25 06:35:18 +0600

    dene creating student features

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student = req.body.student;
+    const result = await StudentServices.createStudentIntoDB(student);
+    res.status(200).json({
+      success: true,
+      message: "Student created successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDb();
+    res.status(200).json({
+      success: true,
+      message: "Students are retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentsFromDb(studentId);
+    res.status(200).json({
+      success: true,
+      message: "Students is retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
