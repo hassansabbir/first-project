2f92b02bb7c6635d2d0271eddf99e65bda84e4fa
hassansabbir <mahmoodsabbir3087@gmail.com>
2024-02-05 00:08:51 +0600

    course service & controller added

---
+++ src/app/modules/Course/course.controller.ts
@@ -0,0 +1,72 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { CourseServices } from "./course.service";
+
+const createCourse = catchAsync(async (req, res, next) => {
+  try {
+    const result = await CourseServices.createCourseIntoDb(req.body);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Course is created successfully",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+const getAllCourses = catchAsync(async (req, res) => {
+  const result = await CourseServices.getAllCourseFromDb();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Courses are retrieved successfully",
+    data: result,
+  });
+});
+
+const getSingleCourse = catchAsync(async (req, res) => {
+  const { facultyId } = req.params;
+  const result = await CourseServices.getSingleCourseFromDb(facultyId);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Course is retrieved successfully",
+    data: result,
+  });
+});
+
+// const updateAcademicFaculty = catchAsync(async (req, res) => {
+//   const { facultyId } = req.params;
+//   const result = await AcademicFacultyServices.updateAcademicFacultyIntoDb(
+//     facultyId,
+//     req.body
+//   );
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: "Academic semester is updated successfully",
+//     data: result,
+//   });
+// });
+
+const deleteCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await CourseServices.deleteCourseFromDb(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Course is updated successfully",
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {
+  createCourse,
+  getAllCourses,
+  getSingleCourse,
+  deleteCourse,
+};
---
+++ src/app/modules/Course/course.service.ts
@@ -0,0 +1,33 @@
+import { TCourse } from "./course.interface";
+import { Course } from "./course.model";
+
+const createCourseIntoDb = async (payload: TCourse) => {
+  const result = await Course.create(payload);
+  return result;
+};
+
+const getAllCourseFromDb = async () => {
+  const result = await Course.find();
+  return result;
+};
+
+const getSingleCourseFromDb = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
+
+const deleteCourseFromDb = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDelete: true },
+    { new: true }
+  );
+  return result;
+};
+
+export const CourseServices = {
+  createCourseIntoDb,
+  getAllCourseFromDb,
+  getSingleCourseFromDb,
+  deleteCourseFromDb,
+};
